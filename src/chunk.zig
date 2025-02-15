38ea2e50c82f6634bd1a8910c572c2b2df03134e
Avram Eisner <150712913+avram-at-hazel@users.noreply.github.com>
2025-02-14 16:05:30 -0800

    added ConstantLong op

--- src/chunk.zig
+++ src/chunk.zig
@@ -4,6 +4,7 @@ const Value = @import("./value.zig").Value;
 
 pub const OpCode = enum(u8) {
     Constant,
+    ConstantLong,
     Return,
 };
 
@@ -43,8 +44,29 @@ pub const Chunk = struct {
         }
     }
 
+    pub fn writeConstant(chunk: *Chunk, value: Value, line: u16) !void {
+        if (chunk.constants.items.len > std.math.maxInt(u8)) {
+            const constIdx = try chunk.addConstantLong(value);
+            const constIdxBytes = std.mem.asBytes(&constIdx);
+            try chunk.write(@intFromEnum(OpCode.ConstantLong), line);
+            for (constIdxBytes) |byte| {
+                try chunk.write(byte, line);
+            }
+        } else {
+            const constIdx = try chunk.addConstant(value);
+            try chunk.write(@intFromEnum(OpCode.Constant), line);
+            try chunk.write(constIdx, line);
+        }
+    }
+
     pub fn addConstant(chunk: *Chunk, value: Value) !u8 {
         const index: u8 = @intCast(chunk.constants.items.len);
+        try chunk.constants.append(value);
+        return index;
+    }
+
+    pub fn addConstantLong(chunk: *Chunk, value: Value) !u24 {
+        const index: u24 = @intCast(chunk.constants.items.len);
         try chunk.constants.append(value);
         return index;
     }
@@ -72,6 +94,7 @@ pub const Chunk = struct {
         const instruction: u8 = chunk.code.items[offset];
         switch (@as(OpCode, @enumFromInt(instruction))) {
             .Constant => return constantInstruction("OP_CONSTANT", chunk, offset),
+            .ConstantLong => return constantLongInstruction("OP_CONSTANT_LONG", chunk, offset),
             .Return => return simpleInstruction("OP_RETURN", offset),
             // else => {
             //     std.debug.print("Unknown opcode %d\n", instruction);
@@ -103,6 +126,19 @@ fn constantInstruction(name: []const u8, chunk: *Chunk, offset: u32) u32 {
     printValue(chunk.constants.items[constantIdx]);
     std.debug.print("'\n", .{});
     return offset + 2;
+}
+
+fn constantLongInstruction(name: []const u8, chunk: *Chunk, offset: u32) u32 {
+    const constantIdxBytes = chunk.code.items[offset + 1 .. offset + 4];
+    const constantIdx = combine_u8_to_u24(constantIdxBytes);
+    std.debug.print("{s: <16} {d: >4} '", .{ name, constantIdx });
+    printValue(chunk.constants.items[constantIdx]);
+    std.debug.print("'\n", .{});
+    return offset + 4;
+}
+
+fn combine_u8_to_u24(bytes: []u8) u24 {
+    return (@as(u24, bytes[0]) << 16) | (@as(u24, bytes[1]) << 8) | @as(u24, bytes[2]);
 }
 
 fn printValue(value: Value) void {
--- src/main.zig
+++ src/main.zig
@@ -12,15 +12,8 @@ pub fn main() !void {
     var chunk = Chunk.init(allocator);
     defer chunk.deinit();
 
-    const val1 = Value{ .Number = 1.2 };
-    const constantIdx1 = chunk.addConstant(val1) catch unreachable;
-    try chunk.write(@intFromEnum(OpCode.Constant), 1);
-    try chunk.write(constantIdx1, 1);
-
-    const val2 = Value{ .Number = 3.1415 };
-    const constantIdx2 = chunk.addConstant(val2) catch unreachable;
-    try chunk.write(@intFromEnum(OpCode.Constant), 2);
-    try chunk.write(constantIdx2, 2);
+    try chunk.writeConstant(Value{ .Number = 1.2 }, 1);
+    try chunk.writeConstant(Value{ .Number = 3.1415 }, 2);
 
     try chunk.write(@intFromEnum(OpCode.Return), 2);
     try chunk.write(@intFromEnum(OpCode.Return), 3);
